0277c10b3f609fa5a2f2be04e25946d30854b3d9
Tsai-Ying <wings0114@gmail.com>
2020-08-04 11:30:51 +0800

    php

---
+++ __html_footer.php
@@ -0,0 +1,21 @@
+<!-- footer -->
+<footer class="flex">
+    <div class="footer-icon flex">
+        <a class="icon-wrapper" href=""><img src="images/icon-fb.svg" alt=""></a>
+        <a class="icon-wrapper" href=""><img src="images/icon-youtube.svg" alt=""></a>
+        <a class="icon-wrapper" href=""><img src="images/icon-twitter.svg" alt=""></a>
+        <a class="icon-wrapper" href=""><img src="images/icon-ig.svg" alt=""></a>
+    </div>
+    <h6>Copyright© WATZ Company All rights Reserved.</h6>
+    <img class="bg-footer flex" src="images/footer.svg" alt="">
+</footer>
+
+<!-- mobile show list -->
+<div class="mobile-show fixedlist">
+    <div class="fixedlist-icon flex">
+        <a class="icon-wrapper" href=""><img src="images/icon-faq.svg" alt=""></a>
+        <a class="icon-wrapper" href=""><img src="images/icon-sock.svg" alt=""></a>
+        <a class="icon-wrapper" href=""><img src="images/icon-member.svg" alt=""></a>
+        <a class="icon-wrapper" href="cart.html"><img src="images/icon-cart.svg" alt=""></a>
+    </div>
+</div>
