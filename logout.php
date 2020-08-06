25de7b446d38bfab3d62da5da93cea8f911f2694
Tsai-Ying <wings0114@gmail.com>
2020-08-06 20:43:35 +0800

    Update cart-payment1.php

--- logout.php
+++ logout.php
@@ -4,7 +4,8 @@ session_start();
 // 清除某個 session 變數
 unset($_SESSION['member']);
 
-// session_destroy(); // 清除所有的 session
+// session_destroy(); 
+// 清除所有的 session
 
 header('Location: member-login-signup.php');
 
