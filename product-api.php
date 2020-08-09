6b5e4d679f08091a2345f674fbfde077ae5ee863
Blair-kkkk <stnina98@gmail.com>
2020-08-09 15:55:32 +0800

    json+append

--- product-api.php
+++ product-api.php
@@ -16,13 +16,14 @@ $cate_id = isset($_GET['cate']) ? intval($_GET['cate']) : 0;
 
 $where = " WHERE 1";
 if(!empty($_GET['series'])){
-    $where .= sprintf(" AND series IN (%s) ", implode(',', $_GET['series']));
+    $where .= sprintf(" AND `series` IN (%s) ", implode(',', $_GET['series']));
 }
-
+if(!empty($_GET['colors'])){
+    $where .= sprintf(" AND `color` IN (%s) ", implode(',', $_GET['colors']));
+}
 if(!empty($_GET['types'])){
     $where .= sprintf(" AND `type` IN (%s) ", implode(',', $_GET['types']));
 }
-
 
 
 $totalPages = 0;
@@ -30,23 +31,20 @@ $t_sql = "SELECT COUNT(1) FROM `product` $where";
 $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 
 
-
-
-// if ($totalRows > 0) {
-//     $totalPages = ceil($totalRows / $perPage);
-//     if ($page < 1) {
-//         header('Location: product.php');
-//         exit;
-//     }
-//     if ($page > $totalPages) {
-//         header('Location: product.php?page=' . $totalPages);
-//         exit;
-//     }
+if ($totalRows > 0) {
+    $totalPages = ceil($totalRows / $perPage);
+    if ($page < 1) {
+        header('Location: product.php');
+        exit;
+    }
+    if ($page > $totalPages) {
+        header('Location: product.php?page=' . $totalPages);
+        exit;
+    }
    
     // $sql = sprintf("SELECT * FROM `product` WHERE series IN (1,2) AND color IN (1) AND type IN (1,2)", $where, ($page - 1) * $perPage, $perPage);
     $sql = sprintf("SELECT * FROM `product` %s LIMIT %s, %s", $where, ($page - 1) * $perPage, $perPage);
 
-    // $sql = sprintf("SELECT * FROM `product` WHERE LIMIT %s, %s", $where, ($page - 1) * $perPage, $perPage);
     $output['rows'] = $pdo->query($sql)->fetchAll();
 }
 
