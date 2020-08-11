cc28ceea0b9d404ee3342c9000b0fc0b5362bb3d
ollylee8520 <67231498+ollylee8520@users.noreply.github.com>
2020-08-11 20:39:08 +0800

    0811 20:11 update (差rwd)

--- project-irregular.php
+++ project-irregular.php
@@ -1,150 +1,375 @@
 <?php require __DIR__ . '/__connect_db.php';
-$pageName = 'project summer';  // 這裡放你的pagename
+$pageName = 'aboutWATZ';  // 這裡放你的pagename
 ?>
 <?php include __DIR__ . '/__html_head.php' ?>
 
-<!-- font-family: 'Josefin Sans', sans-serif; -->
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
-
+<!-- 自己另外的CSS插件 <link> 請放這邊 (nav.css及google fonts共用的不用放) -->
 
 <style>
-    .container {
-        width: 100vw;
-        min-height: 100vh;
-        user-select: none;
-    }
-
-    .bg-left, .bg-right {
-        width: calc(100vw / 2);
-        height: 100vh;
-        position: relative;
-        z-index: -1;
-    }
-
-    .absolute {
-        position: absolute;
-    }
-
-    .bg-blue, .bg-red {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-
-    .img-dot1 {
-        width: 180px;
-        top: 0;
-        left: 80px;
-        mix-blend-mode: multiply;
-    }
-    
-    .img-text {
-        width: 60px;
-        top: 160px;
-        left: 0;
-        position: fixed;
-        z-index: 1;
-    }
-
-    .img-pineapple {
-        width: 460px;
-        bottom: 0;
-        left: -50px;
-        mix-blend-mode: multiply;
-    }
-
-    .img-whiteline {
-        width: 420px;
-        top: 0;
-        right: 0;
-    }
-
-    .wrapper {
-        width: 1200px;
-        justify-content: space-between;
-        padding: 120px 0;
-    }
-
-    .btn-top {
-        z-index: 1;
-    }
+        /* -----------------empty-cart---------------        */
 
 
-
-    @media screen and (max-width: 1280px) {
-        .wrapper {
-            width: 950px;
-            padding: 120px 20px;
+        .container {
+            width: 100vw;
+            min-height: 100vh;
+            user-select: none;
+            overflow: hidden;
         }
-    }
 
-    @media screen and (max-width: 992px) {
-        .wrapper {
-            width: 570px;
-            padding: 120px 0;
+        .block1 {
+            position: relative;
             flex-direction: column;
+            align-items: center;
             justify-content: center;
+        }
+
+        .bg-1 {
+            width: 100vw;
+            height: 100vh;
+
+        }
+
+        .bg-1 img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .logo {
+            width: 450px;
+            position: absolute;
+        }
+
+        .logo img {
+            width: 100%;
+            height: 100%;
+            filter: drop-shadow(8px 8px 6px rgba(105, 105, 105, 0.7));
+        }
+
+        .scroll-down {
+            position: absolute;
+            flex-direction: column;
+            right: 50px;
+            bottom: 30px;
+            animation: mouse-scroll 1s infinite alternate;
+        }
+        @-webkit-keyframes mouse-scroll {
+            to {
+                transform: translate(0, -30px);
+            }
+            
+        }
+
+        .scroll-down h3 {
+            writing-mode: vertical-lr;
+            margin-bottom: 20px;
+        }
+
+        .scroll-down img {
+            width: 30px;
+        }
+
+        .block2 {
+            width: 100vw;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .bg-line {
+            /* border: 1px solid blue; */
             align-items: center;
         }
 
-    }
-
-    @media screen and (max-width: 576px) {
-        .wrapper {
-            width: 100vw;
-            padding: 80px 0 0 0;
+        .bg-line li {
+            width: calc(100vw / 4);
+            height: calc(100vw / 4);
         }
 
+        .bg-line li img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
 
-    }
-</style>
+        }
 
+        .center-frame {
+            position: absolute;
+            flex-direction: column;
+            justify-content: space-between;
+
+        }
+
+        .watzbox {
+            width: 450px;
+            height: 450px;
+            margin-bottom: 40px;
+        }
+
+        .watzbox img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .seemore {
+            width: 200px;
+            height: 45px;
+            background-color: #0388A6;
+            position: absolute;
+            right: -20px;
+            bottom: -10px;
+            outline: none;
+            box-shadow: none;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .seemore h3 {
+            color: #ffffff;
+        }
+
+        .seemore:hover {
+            background-color: #FF9685;
+        }
+
+        .block3 {
+            /* width: 100vw;
+            height: 100vh; */
+            align-items: center;
+            justify-content: center;
+            position: relative;
+        }
+
+        .bg-3 {
+            width: 100vw;
+            height: 100vh;
+            justify-content: flex-start;
+        }
+
+        .bg-3 img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .block3 .pic {
+            width: 550px;
+            position: absolute;
+            z-index: 1;
+        }
+
+        .path-up {
+            position: absolute;
+            left: 30px;
+            top: 0;
+            width: 700px;
+        }
+
+        .path-down {
+            position: absolute;
+            right: 0px;
+            bottom: 0;
+            width: 700px;
+        }
+
+        .block3 .seemore {
+            position: absolute;
+            right: 15%;
+            bottom: 30%;
+            background-color: #B95376;
+        }
+        .block3 .seemore:hover{
+            background-color: #0388A6;
+        }
+
+        .block4 {
+            height: 100vh;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .bg4 {
+            width: 55vw;
+            height: 100vh;
+            position: absolute;
+            right: 0;
+        }
+
+        .bg4 img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        .info{
+            height: 100vh;
+            /* position: absolute; */
+            z-index: 1;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            /* border: 1px solid greenyellow; */
+        }
+        .info img{
+            width: 450px;
+        }
+        .info .title{
+            align-items: center;
+            justify-content: center;
+            margin: 20px 0;
+        }
+        .info .title h1{
+            position: absolute;
+            color: #E7EC99;
+        }
+        .title img{
+            width: 650px;
+            height: 100px;
+        }
+        .info .style1{
+            transform: translateX(-60%);
+        }
+        .block5{
+            width: 100vw;
+            height: 100vh;
+        }
+        .block5 img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        footer{
+            position: absolute;
+            bottom: 0;
+        }
+
+    </style>
 
 <div class="container flex">
+    <!-- 如果container有其它class要自己加上 -->
+
     <?php include __DIR__ . '/__navbar.php' ?>
     <?php include __DIR__ . '/__html_btn-top.php' ?>
-    <div class="flex">
-        <div class="">
-            <img class="" src="images/summer-bgblue.svg" alt="">
+
+    <div class="block1 flex">
+            <div class="bg-1">
+                <img src="images/irregular/kv-web.jpg" alt="">
+            </div>
+            <div class="logo">
+                <img src="images/irregular/WATZ_irregular_LOGO.png" alt="">
+            </div>
+            <div class="scroll-down flex">
+                <h3>SCROLL</h3>
+                <img src="images/irregular/scroll-down.svg" alt="">
+            </div>
         </div>
-        
-    </div>
+        <div class="block2 flex">
+            <ul class="bg-line flex">
+                <li class="">
+                    <img src="images/irregular/square1-1.jpg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square1-2.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square1-3.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square1-4.jpg" alt="">
+                </li>
+            </ul>
+            <ul class="bg-line flex">
+                <li class="">
+                    <img src="images/irregular/square2-1.jpg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square2-2.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square2-3.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square2-4.jpg" alt="">
+                </li>
+            </ul>
+            <ul class="bg-line flex">
+                <li class="">
+                    <img src="images/irregular/square3-1.jpg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square3-2.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square3-3.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square3-4.jpg" alt="">
+                </li>
+            </ul>
+            <ul class="bg-line flex">
+                <li class="">
+                    <img src="images/irregular/square4-1.jpg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square4-2.svg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square4-3.jpg" alt="">
+                </li>
+                <li class="">
+                    <img src="images/irregular/square4-4.jpg" alt="">
+                </li>
+            </ul>
+            <div class="center-frame flex">
+                <div class="watzbox">
+                    <img id="watzbox" src="images/watzbox2-1.png" alt="">
+                </div>
+                <button class="seemore btn-coral flex">
+                    <h3>See More</h3>
+                </button>
+            </div>
+        </div>
+        <div class="block3 flex">
+            <div class="bg-3 flex">
+                <img src="images/irregular/bg3-left.svg" alt="">
+                <img src="images/irregular/bg3-right.svg" alt="">
+            </div>
+            <img class="path-up" src="images/irregular/Path-down.svg" alt="">
+            <img class="pic" src="images/irregular/WATZ_irregular.png" alt="">
+            <img class="path-down" src="images/irregular/Path-up.svg" alt="">
+            <button class="seemore btn-coral flex">
+                <h3>See More</h3>
+            </button>
+        </div>
+        <div class="block4 flex">
+            <div class="bg4 flex">
+                <img src="images/irregular/popcorn.jpg" alt="">
+            </div>
+            <div class="info flex">
+                <img class="style1" src="images/irregular/style1.jpg" alt="">
+                <div class="title flex">
+                    <h1>styling yourself</h1>
+                    <img src="images/irregular/brush.svg" alt="">
+                </div>
+                <img src="images/irregular/style2.jpg" alt="">
+            </div>
+        </div>
+        <div class="block5">
+                <img src="images/irregular/bg5.jpeg" alt="">
 
-    <div class="wrapper flex transition">
+        </div>
 
-    </div>
     <?php include __DIR__ . '/__html_footer.php' ?>
 </div>
 <?php include __DIR__ . '/__scripts.php' ?>
 
 <script>
-    /* // php 加入購物車
-
-    const buy_btn = $('.buy_btn');
-
-    buy_btn.click(function() {
-        const p_item = $('.p_item');
-        const sid = p_item.attr('data-sid');
-        const qty = $('.qty').val();
-        const sendObj = {
-            action: 'add',
-            sid,
-            qty: qty
-        }
-        $.get('cart-handle.php', sendObj, function(data) {
-            console.log(data);
-            setCartCount(data);
-        }, 'json');
-
-        // alert(sid + ',' + qty)
-        // 彈跳視窗
-        $(".notice").addClass("animate__animated animate__flipInX animate__faster");
-        $(".notice").addClass("success");
-        setTimeout(function() {
-            $(".notice").removeClass("success");
-            $(".notice").removeClass("animate__animated animate__flipInX animate__faster");
-        }, 800);
-    }); */
+    $("#watzbox").hover(function() {
+        $(this).attr("src", "images/watzbox2-2.png");
+    }, function() {
+        $(this).attr("src", "images/watzbox2-1.png");
+    })
 </script>
 
 <?php require __DIR__ . '/__html_foot.php' ?>
