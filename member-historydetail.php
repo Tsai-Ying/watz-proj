42c1e3e8528c21fc33dc64e5b236d5f1829a34eb
tonie0211 <wty02110@gmail.com>
2020-08-18 10:51:45 +0800

    delete pagename

--- member-historydetail.php
+++ member-historydetail.php
@@ -1,5 +1,5 @@
 <?php require __DIR__ . '/__connect_db.php';
-$pageName = 'aboutWATZ';  // 這裡放你的pagename
+$pageName = '';  // 這裡放你的pagename
 
 $id = isset($_SESSION['member']['id']) ? intval($_SESSION['member']['id']) : 0;
 $sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
